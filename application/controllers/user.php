8d21c6e71b4e969682ac3d3cb4bf69e9a5744ab6
Arum Puspa Pratiwi <arum_puspa_26 rpl@student.smktelkom-mlg.sch.id>
2019-01-22 14:09:06 +0700

    selesai

---
+++ application/controllers/user.php
@@ -0,0 +1,14 @@
+<?php
+  defined('BASEPATH') OR exit ('No direct script access allowed');
+
+  class user extends CI_Controller {
+    public function index()
+    {
+      $data['konten']="home";
+      $this->load->view('template', $data);
+    }
+  }
+
+/* End of file Template.php */
+/* Location: ./application/controllers/Template.php */
+?>
